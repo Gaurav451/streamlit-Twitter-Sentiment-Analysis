4b417a2e948653b7e07ccf842f21ef9081e30160
Gaurav Sharma <69210537+Gaurav451@users.noreply.github.com>
2023-04-28 19:31:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ else :
         st.sidebar.subheader("When and where are users tweeting from?")
         hour = st.sidebar.slider("Hour to look at", 0, 23)
         modified_data = data[data['tweet_created'].dt.hour == hour]
-        if not st.sidebar.checkbox("Close", True, key = random.sample(range(1000, 9999), len(data.columns))):
+        if not st.sidebar.checkbox("Close", True, key = 23):
             st.markdown("### Tweet locations based on time of day")
             st.markdown("%i tweets between %i:00 and %i:00" % (len(modified_data), hour, (hour + 1) % 24))
             st.map(modified_data)
@@ -83,7 +83,7 @@ else :
         each_airline = st.sidebar.selectbox('Visualization type', ['Bar plot', 'Pie chart'], key='2')
         airline_sentiment_count = data.groupby('airline')['airline_sentiment'].count().sort_values(ascending=False)
         airline_sentiment_count = pd.DataFrame({'Airline':airline_sentiment_count.index, 'Tweets':airline_sentiment_count.values.flatten()})
-        if not st.sidebar.checkbox("Close", True, key = random.sample(range(1000, 9999), len(data.columns))):
+        if not st.sidebar.checkbox("Close", True, key = 76):
             if each_airline == 'Bar plot':
                 st.subheader("Total number of tweets for each airline")
                 fig_1 = px.bar(airline_sentiment_count, x='Airline', y='Tweets', color='Tweets', height=500)
@@ -140,7 +140,7 @@ else :
 
         st.sidebar.header("Word Cloud")
         word_sentiment = st.sidebar.radio('Display word cloud for what sentiment?', ('positive', 'neutral', 'negative'))
-        if not st.sidebar.checkbox("Close", True, key = random.sample(range(1000, 9999), len(data.columns))):
+        if not st.sidebar.checkbox("Close", True, key = 6):
             st.subheader('Word cloud for %s sentiment' % (word_sentiment))
             df = data[data['airline_sentiment']==word_sentiment]
             words = ' '.join(df['text'])
@@ -149,5 +149,6 @@ else :
             plt.imshow(wordcloud)
             plt.xticks([])
             plt.yticks([])
+            st.set_option('deprecation.showPyplotGlobalUse', False)
             st.pyplot()
 
